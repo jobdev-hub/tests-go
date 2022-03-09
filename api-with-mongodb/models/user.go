cbf8efd1f894abd0c2ff71868980c65762436845
jobdev-hub <job.methods@outlook.com>
2022-03-09 18:25:21 -0300

    included new fields and update func CheckFields

--- api-with-mongodb/models/user.go
+++ api-with-mongodb/models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strings"
 	"time"
 )
 
@@ -10,18 +11,33 @@ type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Name      string             `json:"name"`
 	Email     string             `json:"email"`
+	Roles     []string           `json:"roles"`
+	Active    bool               `json:"active"`
 	CreatedAt time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
 	UpdatedAt *time.Time         `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
 
 type Users []*User
 
-func VerifyFields(user User) error {
+func CheckFields(user User) error {
+
+	var err []string
+
 	if user.Name == "" {
-		return errors.New("name is required")
+		err = append(err, "name")
 	}
+
 	if user.Email == "" {
-		return errors.New("name is required")
+		err = append(err, "email")
 	}
+
+	if user.Roles == nil {
+		err = append(err, "roles")
+	}
+
+	if err != nil {
+		return errors.New("[" + strings.Join(err, ", ") + "] is required")
+	}
+
 	return nil
 }
